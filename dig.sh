1b4ba95ce0354bf12881502e6655717719cc322c
MatrixEvo <39986709+MatrixEvo@users.noreply.github.com>
2022-03-23 16:06:26 +0800

    Sort the output

--- dig.sh
+++ dig.sh
@@ -6,13 +6,13 @@ while [ 1 ]; do
 		read -p "Input Hostname : " HOSTNAME </dev/tty
 		[ -z "$HOSTNAME" ] && echo "Please Input Hostname..." || break
 	done
-	NS_RECORD=$(dig +short ns $HOSTNAME @8.8.8.8)
-	A_RECORD=$(dig +short a $HOSTNAME @8.8.8.8)
-	MX_RECORD=$(dig +short MX $HOSTNAME @8.8.8.8)
-	MAIL_RECORD=$(dig +short a mail.$HOSTNAME @8.8.8.8)
-	WEBMAIL_RECORD=$(dig +short a webmail.$HOSTNAME @8.8.8.8)
-	TXT_RECORD=$(dig +short txt $HOSTNAME @8.8.8.8)
-	[ -z "$A_RECORD" ] || PTR_RECORD=$(dig -x $A_RECORD @8.8.8.8 | grep "PTR")
+	NS_RECORD=$(dig +short ns $HOSTNAME @8.8.8.8 | sort)
+	A_RECORD=$(dig +short a $HOSTNAME @8.8.8.8 | sort)
+	MX_RECORD=$(dig +short MX $HOSTNAME @8.8.8.8 | sort)
+	MAIL_RECORD=$(dig +short a mail.$HOSTNAME @8.8.8.8 | sort)
+	WEBMAIL_RECORD=$(dig +short a webmail.$HOSTNAME @8.8.8.8 | sort)
+	TXT_RECORD=$(dig +short txt $HOSTNAME @8.8.8.8 | sort)
+	[ -z "$A_RECORD" ] || PTR_RECORD=$(dig -x $A_RECORD @8.8.8.8 | grep "PTR" | sort)
 	[ -z "$A_RECORD" ] || IPINFO_A=$(curl -s ipinfo.io/$A_RECORD | grep "\"org\":" | xargs | cut -f1 -d ",")
 	[ -z "$MAIL_RECORD" ] || IPINFO_MAIL=$(curl -s ipinfo.io/$MAIL_RECORD | grep "\"org\":" | xargs | cut -f1 -d ",")
 	[ -z "$WEBMAIL_RECORD" ] || IPINFO_WEBMAIL=$(curl -s ipinfo.io/$WEBMAIL_RECORD | grep "\"org\":" | xargs | cut -f1 -d ",")
