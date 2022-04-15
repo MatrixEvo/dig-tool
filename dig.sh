954314d84f253f10f23495dd4d081be666376bcb
MatrixEvo <39986709+MatrixEvo@users.noreply.github.com>
2022-04-15 22:16:30 +0800

    Update dig.sh

--- dig.sh
+++ dig.sh
@@ -9,13 +9,13 @@ lightred="\033[1;31m"
 yellow="\033[1;33m"
 
 header() { echo -e "${blue}${1}${end}" ; }
-records() { if [[ "${1}" =~ [.]|"PTR" ]]; then echo -e "${darkyellow}${1}${end}" ; fi ; }
+records() { echo -e "${darkyellow}${1}${end}" ; }
 ipinfo_records() { echo -e "${lightred}${1}${end}" ; }
 yellow() { echo -e "${yellow}${1}${end}" ; }
 red() { echo -e "${red}${1}${end}" ; }
 
 while true; do
-  unset hostname ns_record a_record mx_record mail_record webmail_record txt_record ptr_record ipinfo_a_record ipinfo_mail_record ipinfo_webmail_record
+  unset hostname ns_record a_record mx_record mail_record webmail_record txt_record ptr_a_record ptr_a ptr_mail_record ptr_mail ptr_webmail_record ptr_webmail ipinfo_a_record ipinfo_mail_record ipinfo_webmail_record
   while true; do
     read -r -p "$(yellow "Input Hostname : ")" hostname </dev/tty
     if [[ -z ${hostname} ]]; then red "Please Input Hostname..." ; else break ; fi
@@ -26,7 +26,9 @@ while true; do
   mail_record=$(dig +short a mail."${hostname}" @8.8.8.8 | sort)
   webmail_record=$(dig +short a webmail."${hostname}" @8.8.8.8 | sort)
   txt_record=$(dig +short txt "${hostname}" @8.8.8.8 | sort)
-  [[ -z ${a_record} ]] || ptr_record=$(dig -x "$(echo "${a_record}" | head -n1)" @8.8.8.8 | grep "PTR" | grep -v ";" | sort)
+  [[ -z ${a_record} ]] || ptr_a_record=$(echo "${a_record}" | while read -r ptr_a; do dig -x "${ptr_a}" @8.8.8.8 | grep "PTR" | grep -v ";" ; done)
+  [[ -z ${mail_record} ]] || ptr_mail_record=$(echo "${mail_record}" | while read -r ptr_mail; do dig -x "${ptr_mail}" @8.8.8.8 | grep "PTR" | grep -v ";" ; done)
+  [[ -z ${webmail_record} ]] || ptr_webmail_record=$(echo "${webmail_record}" | while read -r ptr_webmail; do dig -x "${ptr_webmail}" @8.8.8.8 | grep "PTR" | grep -v ";" ; done)
   [[ -z ${a_record} ]] || ipinfo_a_record=$(curl -s ipinfo.io/"$(echo "${a_record}" | head -n1)" | grep "\"org\":" | xargs | cut -f1 -d ",")
   [[ -z ${mail_record} ]] || ipinfo_mail_record=$(curl -s ipinfo.io/"$(echo "${mail_record}" | head -n1)" | grep "\"org\":" | xargs | cut -f1 -d ",")
   [[ -z ${webmail_record} ]] || ipinfo_webmail_record=$(curl -s ipinfo.io/"$(echo "${webmail_record}" | head -n1)" | grep "\"org\":" | xargs | cut -f1 -d ",")
@@ -36,7 +38,7 @@ while true; do
   echo
   header "A record for ${hostname}"
   [[ -z ${a_record} ]] || records "${a_record}"
-  [[ -z ${a_record} ]] || echo "${a_record}" | while read ptr_a_record; do records "$(dig -x "${ptr_a_record}" @8.8.8.8 | grep "PTR" | grep -v ";")" ; done
+  if [[ "${ptr_a_record}" =~ "PTR" ]]; then records "${ptr_a_record}" ; fi
   [[ -z ${ipinfo_a_record} ]] || ipinfo_records "${ipinfo_a_record}"
   echo
   header "MX record for ${hostname}"
@@ -44,12 +46,12 @@ while true; do
   echo
   header "A record for mail.${hostname}"
   [[ -z ${mail_record} ]] || records "${mail_record}"
-  [[ -z ${mail_record} ]] || echo "${mail_record}" | while read ptr_mail_record; do records "$(dig -x "${ptr_mail_record}" @8.8.8.8 | grep "PTR" | grep -v ";")" ; done
+  if [[ "${ptr_mail_record}" =~ "PTR" ]]; then records "${ptr_mail_record}" ; fi
   [[ -z ${ipinfo_mail_record} ]] || ipinfo_records "${ipinfo_mail_record}"
   echo
   header "A record for webmail.${hostname}"
   [[ -z ${webmail_record} ]] || records "${webmail_record}"
-  [[ -z ${webmail_record} ]] || echo "${webmail_record}" | while read ptr_webmail_record; do records "$(dig -x "${ptr_webmail_record}" @8.8.8.8 | grep "PTR" | grep -v ";")" ; done
+  if [[ "${ptr_webmail_record}" =~ "PTR" ]]; then records "${ptr_webmail_record}" ; fi
   [[ -z ${ipinfo_webmail_record} ]] || ipinfo_records "${ipinfo_webmail_record}"
   echo
   header "TXT record for ${hostname}"
