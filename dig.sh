d885f93a7706f1d4acbf2c5fc68dc6eabe2bbe3d
MatrixEvo <39986709+MatrixEvo@users.noreply.github.com>
2022-05-03 02:44:18 +0800

    Update dig.sh

--- dig.sh
+++ dig.sh
@@ -16,12 +16,25 @@ ipinfo_records() { echo -e "${lightred}${1}${end}" ; }
 yellow() { echo -e "${yellow}${1}${end}" ; }
 red() { echo -e "${red}${1}${end}" ; }
 check_valid_ip() { grep -oE "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])" ; }
-ipinfo_org_only() { grep "\"org\":" | xargs | cut -f1 -d "," ; }
+ipinfo_org_only() { grep "\"org\":" | xargs | cut -d',' -f1 ; }
 
 check_hostname() {
-  local ns_record a_record mx_record mail_record webmail_record txt_record ptr_a_record ptr_a ptr_mail_record ptr_mail ptr_webmail_record ptr_webmail ipinfo_a_record ipinfo_mail_record ipinfo_webmail_record
+  local hostnamedotcount roothostname ns_record a_record mx_record mail_record webmail_record txt_record ptr_a_record ptr_a ptr_mail_record ptr_mail ptr_webmail_record ptr_webmail ipinfo_a_record ipinfo_mail_record ipinfo_webmail_record
   # GATHERING INFO
-  ns_record=$(dig +short ns "${hostname}" @8.8.8.8 | sort)
+  hostnamedotcount=$(echo "${hostname}" | grep -o "\." | wc -l)
+  if [[ ${hostnamedotcount} -gt 1 ]]; then
+    roothostname=$(echo "${hostname}" | cut -d'.' -f"${hostnamedotcount}"-)
+    if [[ ${#roothostname} == 6 ]]; then
+      ns_record=$(echo "${hostname}" | cut -d'.' -f"$(("${hostnamedotcount}" - 1))"- | xargs dig +short ns @8.8.8.8 | sort)
+      roothostname=$(echo "${hostname}" | cut -d'.' -f"$(("${hostnamedotcount}" - 1))"-)
+    else
+      ns_record=$(echo "${hostname}" | cut -d'.' -f"${hostnamedotcount}"- | xargs dig +short ns @8.8.8.8 | sort)
+      roothostname=$(echo "${hostname}" | cut -d'.' -f"${hostnamedotcount}"-)
+    fi
+  else
+    ns_record=$(dig +short ns "${hostname}" @8.8.8.8 | sort)
+    roothostname="${hostname}"
+  fi
   a_record=$(dig +short a "${hostname}" @8.8.8.8 | sort)
   mx_record=$(dig +short MX "${hostname}" @8.8.8.8 | sort)
   mail_record=$(dig +short a mail."${hostname}" @8.8.8.8 | sort)
@@ -35,28 +48,28 @@ check_hostname() {
   [[ -z ${webmail_record} ]] || ipinfo_webmail_record=$(curl -s ipinfo.io/"$(echo "${webmail_record}" | head -n1)" | ipinfo_org_only)
   # OUTPUT BELOW
   echo
-  header "NS record for \"${hostname}\""
+  header "NS record for ${roothostname}"
   [[ -z ${ns_record} ]] || records "${ns_record}"
   echo
-  header "A record for \"${hostname}\""
+  header "A record for ${hostname}"
   [[ -z ${a_record} ]] || records "${a_record}"
   [[ ${ptr_a_record} =~ "PTR" ]] && records "${ptr_a_record}"
   [[ -z ${ipinfo_a_record} ]] || ipinfo_records "${ipinfo_a_record}"
   echo
-  header "MX record for \"${hostname}\""
+  header "MX record for ${hostname}"
   [[ -z ${mx_record} ]] || records "${mx_record}"
   echo
-  header "A record for \"mail.${hostname}\""
+  header "A record for mail.${hostname}"
   [[ -z ${mail_record} ]] || records "${mail_record}"
   [[ ${ptr_mail_record} =~ "PTR" ]] && records "${ptr_mail_record}"
   [[ -z ${ipinfo_mail_record} ]] || ipinfo_records "${ipinfo_mail_record}"
   echo
-  header "A record for \"webmail.${hostname}\""
+  header "A record for webmail.${hostname}"
   [[ -z ${webmail_record} ]] || records "${webmail_record}"
   [[ ${ptr_webmail_record} =~ "PTR" ]] && records "${ptr_webmail_record}"
   [[ -z ${ipinfo_webmail_record} ]] || ipinfo_records "${ipinfo_webmail_record}"
   echo
-  header "TXT record for \"${hostname}\""
+  header "TXT record for ${hostname}"
   [[ -z ${txt_record} ]] || records "${txt_record}"
   echo
 }
@@ -69,7 +82,7 @@ check_ip() {
   [[ -z ${a_record} ]] || ipinfo_a_record=$(curl -s ipinfo.io/"$(echo "${a_record}" | head -n1)" | ipinfo_org_only)
   # OUTPUT BELOW
   echo
-  header "A record for \"${ip}\""
+  header "A record for ${ip}"
   [[ -z ${a_record} ]] || records "${a_record}"
   [[ ${ptr_a_record} =~ "PTR" ]] && records "${ptr_a_record}"
   [[ -z ${ipinfo_a_record} ]] || ipinfo_records "${ipinfo_a_record}"
@@ -80,7 +93,7 @@ start() {
   while IFS= read -rep "$(yellow "Input Hostname : ")" inputhostname </dev/tty ; do
     unset ip hostname
     ip=$(echo "${inputhostname}" | check_valid_ip | head -n1)
-    hostname=$(echo "${inputhostname}" | sed 's/[=+,\"<> !@#$%^&*()\/:?;_]/\n/g' | grep "[.]" | head -n1 )
+    hostname=$(echo "${inputhostname}" | sed 's/[=+,\"<> !@#$%^&*()\/:?;_]/\n/g' | grep "\." | head -n1 )
     if [[ -z ${ip} ]] && [[ -z ${hostname} ]] || [[ ${#hostname} -le 3 ]]; then red "Please Input Hostname..." ; break ; fi
     if [[ -n ${ip} ]]; then history -s "${ip}" ; check_ip ; elif [[ -n ${hostname} ]] ; then history -s "${hostname}" ; check_hostname ; fi
   done
